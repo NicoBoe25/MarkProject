8a56c646856ddd56f264d7074123485d6c0d88ee
Boehrer Nicolas <nicolas.boehrer@edu.univ-fcomte.fr>
2020-02-07 14:37:22 +0100

    test avec classe notes

--- MarkProject/Mark.swift
+++ MarkProject/Mark.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+
+class Mark{
+    var name: String
+    var number: Int
+    
+    init(name: String, number: Int) {
+        self.name=name
+        self.number=number
+    }
+}
