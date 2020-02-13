2499eb1953e034ee82d1819e0cc1d2049c64fd56
Thomas <thomas@iut>
2020-02-13 11:52:41 +0100

    add map set pin user location

--- MarkProject/AddEditNoteTableViewController.swift
+++ MarkProject/AddEditNoteTableViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class AddEditNoteTableViewController: UITableViewController {
+class AddEditNoteTableViewController: UITableViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -19,6 +20,8 @@ class AddEditNoteTableViewController: UITableViewController {
     
     @IBOutlet weak var userLocationButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
+    var currentLocationStr = "Current location"
+    var locationManager:CLLocationManager!
     
     
     var note: Note?
@@ -46,9 +49,59 @@ class AddEditNoteTableViewController: UITableViewController {
     }
     
     
-    // Button to set current user location on the map
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let mUserLocation:CLLocation = locations[0] as CLLocation
+        let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
+        let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(mRegion, animated: true)
+
+        // Get user's Current Location and Drop a pin
+    let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
+        mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
+        mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
+        mapView.addAnnotation(mkAnnotation)
+    }
+    
+    //MARK:- Intance Methods
+    func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
+
+        geoCoder.reverseGeocodeLocation(location) {
+            (placemarks, error) -> Void in
+
+            if let mPlacemark = placemarks{
+                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
+                    if let Name = dict["Name"] as? String{
+                        if let City = dict["City"] as? String{
+                            self.currentLocationStr = Name + ", " + City
+                        }
+                    }
+                }
+            }
+        }
+        return currentLocationStr
+    }
+    
+    
+    //MARK:- Intance Methods
+
+    func determineCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    
+    // Button to set current user location on the map
     @IBAction func setUserLocationOnMap(_ sender: Any) {
+        determineCurrentLocation()
     }
     
     // MARK: - Navigation
