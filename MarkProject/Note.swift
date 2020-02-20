d220ac5fd3d54b63a9a7956fcba7371cbabb6d20
Thomas <thomas@iut>
2020-02-20 10:22:50 +0100

    tyui

--- MarkProject/Note.swift
+++ MarkProject/Note.swift
@@ -15,19 +15,22 @@ class Note{
     var content: String
     var date: String
     var local: CLLocation
+    var photo: String
     
     
-    init(title: String, content: String, date: String) {
+    init(title: String, content: String, date: String, photo: String) {
         self.title=title
         self.content=content
         self.date=date
         self.local=CLLocation(latitude: 0, longitude: 0)
+        self.photo=photo
     }
     
-    init(title: String, content: String, date: String, local: CLLocation) {
+    init(title: String, content: String, date: String, local: CLLocation, photo: String) {
         self.title=title
         self.content=content
         self.date=date
         self.local=local
+        self.photo=photo
     }
 }
